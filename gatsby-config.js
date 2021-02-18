7586e2396fc4b517f4a3a959d34f5ad8b362992c
ndjndj <teq.ndj@gmail.com>
2021-02-19 02:46:37 +0900

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,32 +6,52 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pickles`,
+    description: `how to Pickles`,
+    author: `@ndj`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/src/img`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: "gatsby-source-microcms",
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/img/icon_128.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-microcms`,
       options: {
         apiKey: process.env.X_API_KEY,
         serviceId: process.env.SERVICE_ID,
         apis: [
           {
-            endpoint: "locale-info",
+            endpoint: `locale-info`,
+            type: `get`
           },
           {
-            endpoint: "pickle-api",
-          },
-        ],
-      },
-    },
+            endpoint: `pickle-api`,
+            type: `get`,
+          }
+        ]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-
+  ]
 }
