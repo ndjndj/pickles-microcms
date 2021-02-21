b5d11eda9b2bf86f86d8f07efde1092946903b75
ndjndj <teq.ndj@gmail.com>
2021-02-21 21:08:29 +0900

    locale.js

---
+++ src/pages/locale.js
@@ -0,0 +1,36 @@
+import ReleaseNote from '../components/release-note';
+import HowToInstall from '../components/how-to-install';
+import HowToUnInstall from '../components/how-to-uninstall';
+import PrivacyAssurance from '../components/privacy-assurance';
+import Disclaimer from '../components/disclaimer';
+import HowToUse from '../components/how-to-use';
+import Footer from '../components/footer';
+import { graphql, Link } from 'gatsby';
+
+export default function Home({ pageContext }) {
+  const data = pageContext.post;
+  return (
+    <React.Fragment>
+      <Header />
+      <div id="contents">
+        <PageIndex />
+        <AboutPickles />
+        <ReleaseNote />
+        <HowToInstall />
+        <HowToUnInstall />
+        <HowToUse />
+        <PrivacyAssurance />
+        <Disclaimer />
+      </div>
+      <ul>
+        <li>{data.AboutPickle.step1}</li>
+        <li>{data.AboutPickle.step2}</li>
+        <li>{data.AboutPickle.step3}</li>
+        <li>{data.AboutPickle.step4}</li>
+        <li>{data.AboutPickle.step5}</li>
+        <li>{data.AboutPickle.type}</li>
+      </ul>
+      <Footer />
+    </React.Fragment>
+  );
+}
