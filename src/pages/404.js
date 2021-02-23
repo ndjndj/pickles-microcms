fb76f38d99e06960221320d6c6d9402091dbf012
ndjndj <teq.ndj@gmail.com>
2021-02-23 17:07:53 +0900

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
 import React from "react"
 
+import Header from '../components/header';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Footer from '../components/footer';
+
 
 const NotFoundPage = () => (
   <Layout>
+    <Header />
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Footer />
   </Layout>
 )
 
