690ae50c5a25a9476bc7e4ecd6889ce2dc9377ad
ndjndj <teq.ndj@gmail.com>
2021-02-23 17:11:21 +0900

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,17 @@
 import React from "react"
-
 import Header from '../components/header';
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Footer from '../components/footer';
 
 
 const NotFoundPage = () => (
-  <Layout>
+  <React.Fragment>
     <Header />
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     <Footer />
-  </Layout>
+  </ React.Fragment>
 )
 
 export default NotFoundPage
